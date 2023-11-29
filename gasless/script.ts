cca9ba0bee5e73fff7b3000ab98e9543fa02af02
maxwell-1999 <maxwell@buffer.finance>
2023-11-29 18:41:02 +0530

    WIP

--- gasless/script.ts
+++ gasless/script.ts
@@ -36,31 +36,20 @@ export const gaslessSingleTokenTransfer = async () => {
   console.log("sending");
   let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
   let signer = new ethers.Wallet(PRIVATE_KEY, provider);
-  let eoaAddress = signer.address;
-  console.log(`RPC_URL: `, RPC_URL);
-
-  console.log(`API_KEY: `, API_KEY);
   let aarcSDK = new AarcSDK({
     rpcUrl: RPC_URL,
     chainId: 421613,
     apiKey: API_KEY,
   });
-
-  let balances = await aarcSDK.fetchBalances(eoaAddress);
-  console.log(balances);
-
-  let safes = await aarcSDK.getAllSafes(eoaAddress);
-  console.log(safes);
-
-  // await aarcSDK.executeMigrationGasless({
-  //   senderSigner: signer,
-  //   receiverAddress: "0x89e2af300bCf6071068de29AEb92b4f9D97e1f11",
-  //   transferTokenDetails: [
-  //     {
-  //       tokenAddress: "0xda11d2c3b026561cce889ff5a020eae21308058c",
-  //       amount: BigNumber.from("1000000"),
-  //     },
-  //   ],
-  //   gelatoApiKey: GELATO_API_KEY,
-  // });
+  await aarcSDK.executeMigrationGasless({
+    senderSigner: signer,
+    receiverAddress: "0x89e2af300bCf6071068de29AEb92b4f9D97e1f11",
+    transferTokenDetails: [
+      {
+        tokenAddress: "0xda11d2c3b026561cce889ff5a020eae21308058c",
+        amount: BigNumber.from("1000000"),
+      },
+    ],
+    gelatoApiKey: GELATO_API_KEY,
+  });
 };
