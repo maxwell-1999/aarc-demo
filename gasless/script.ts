83c29df4b0595c12fec8d51426d2e5277d8c6604
maxwell-1999 <maxwell@buffer.finance>
2023-11-29 18:34:12 +0530

    WIP

--- gasless/script.ts
+++ gasless/script.ts
@@ -1,10 +1,10 @@
 import { BigNumber, ethers } from "ethers";
-import {AarcSDK} from "aarc-sdk";
+import { AarcSDK } from "aarc-sdk";
 import { RPC_URL, PRIVATE_KEY, API_KEY, GELATO_API_KEY } from "../constants";
 
 export const gaslessTokensTransfer = async () => {
   let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-  let signer = new ethers.Wallet(PRIVATE_KEY, provider);  
+  let signer = new ethers.Wallet(PRIVATE_KEY, provider);
   let eoaAddress = signer.address;
 
   let aarcSDK = new AarcSDK({
@@ -21,40 +21,46 @@ export const gaslessTokensTransfer = async () => {
 
   await aarcSDK.executeMigrationGasless({
     senderSigner: signer,
-    receiverAddress:"0x786E6045eacb96cAe0259cd761e151b68B85bdA7",
-    transferTokenDetails:[
-      {tokenAddress:"0xbb8dB535d685F2742D6e84EC391c63e6a1Ce3593",amount:BigNumber.from("100000000")},
-      {tokenAddress:"0x2055b06db421f17c19c655fd4a1c325e8514af67",amount:BigNumber.from("20000000000000000")},
-      {tokenAddress:"0xf4ca1a280ebccdaebf80e3c128e55de01fabd893",amount:BigNumber.from("500000000")},
-      {tokenAddress:"0x932ca55b9ef0b3094e8fa82435b3b4c50d713043",tokenIds:["28507"]},
+    receiverAddress: "0x89e2af300bCf6071068de29AEb92b4f9D97e1f11",
+    transferTokenDetails: [
+      {
+        tokenAddress: "0xda11d2c3b026561cce889ff5a020eae21308058c",
+        amount: BigNumber.from("1000000"),
+      },
     ],
     gelatoApiKey: GELATO_API_KEY,
-  })
-}
+  });
+};
 
 export const gaslessSingleTokenTransfer = async () => {
+  console.log("sending");
   let provider = new ethers.providers.JsonRpcProvider(RPC_URL);
-  let signer = new ethers.Wallet(PRIVATE_KEY, provider);  
+  let signer = new ethers.Wallet(PRIVATE_KEY, provider);
   let eoaAddress = signer.address;
+  console.log(`RPC_URL: `, RPC_URL);
 
+  console.log(`API_KEY: `, API_KEY);
   let aarcSDK = new AarcSDK({
     rpcUrl: RPC_URL,
-    chainId: 4,
+    chainId: 421613,
     apiKey: API_KEY,
   });
 
-  let balances = await aarcSDK.fetchBalances(eoaAddress, []);
+  let balances = await aarcSDK.fetchBalances(eoaAddress);
   console.log(balances);
 
   let safes = await aarcSDK.getAllSafes(eoaAddress);
   console.log(safes);
 
-  await aarcSDK.executeMigrationGasless({
-    senderSigner: signer,
-    receiverAddress:"0x786E6045eacb96cAe0259cd761e151b68B85bdA7",
-    transferTokenDetails:[
-      {tokenAddress:"0xaff4481d10270f50f203e0763e2597776068cbc5",amount:BigNumber.from("300000000000000")},
-    ],
-    gelatoApiKey: GELATO_API_KEY,
-  })
-}
+  // await aarcSDK.executeMigrationGasless({
+  //   senderSigner: signer,
+  //   receiverAddress: "0x89e2af300bCf6071068de29AEb92b4f9D97e1f11",
+  //   transferTokenDetails: [
+  //     {
+  //       tokenAddress: "0xda11d2c3b026561cce889ff5a020eae21308058c",
+  //       amount: BigNumber.from("1000000"),
+  //     },
+  //   ],
+  //   gelatoApiKey: GELATO_API_KEY,
+  // });
+};
--- index.ts
+++ index.ts
@@ -1,18 +1,13 @@
-import { gaslessTokensTransfer, gaslessSingleTokenTransfer } from "./gasless/script";
-import { allTokensTransfer, tokensTransfer, nativeTokenTransfer, nftTokenTransfer } from "./nongasless/script";
-import { biconomyWallet } from "./nongasless/biconomy";
+import {
+  gaslessTokensTransfer,
+  gaslessSingleTokenTransfer,
+} from "./gasless/script";
 
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-  // await nftTokenTransfer();
-  // await tokensTransfer();
- await gaslessTokensTransfer();
-  // await allTokensTransfer();
-  // await gaslessSingleTokenTransfer();
-  // await nativeTokenTransfer();
-  // await biconomyWallet();
+  await gaslessSingleTokenTransfer();
 }
 
 main();
